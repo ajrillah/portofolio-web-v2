75cf97259936003edfca20a780a0447c5614a3e1
ajrillah <fakhri.ajril@gmail.com>
2025-03-22 06:33:05 +0700

    ADD: Hero Done

---
+++ src/components/Button.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+
+// Node
+import PropTypes from "prop-types";
+
+//
+const ButtonPrimary = ({ href, target = "_self", label, icon, classes }) => {
+  if (href) {
+    return (
+      <a href={href} target={target} className={"btn btn-primary " + classes}>
+        {label}
+
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </a>
+    );
+  } else {
+    return (
+      <button className={"btn btn-primary " + classes} aria-hidden="true">
+        {label}
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </button>
+    );
+  }
+};
+
+ButtonPrimary.PropTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+const ButtonOutline = ({ href, target = "_self", label, icon, classes }) => {
+  if (href) {
+    return (
+      <a href={href} target={target} className={"btn btn-outline " + classes}>
+        {label}
+
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </a>
+    );
+  } else {
+    return (
+      <button className={"btn btn-outline " + classes} aria-hidden="true">
+        {label}
+        {icon ? (
+          <span className="material-symbols-rounded" aria-hidden="true">
+            {icon}
+          </span>
+        ) : undefined}
+      </button>
+    );
+  }
+};
+
+ButtonOutline.PropTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string,
+};
+
+export { ButtonPrimary, ButtonOutline };
