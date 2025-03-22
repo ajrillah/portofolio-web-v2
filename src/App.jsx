6be95f303109f575c74a44c5abe62fb1879d72c5
ajrillah <fakhri.ajril@gmail.com>
2025-03-22 21:49:52 +0700

    ADD: Footer Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// Node Modules
+
 // Components
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -6,6 +8,7 @@ import Skill from "./components/Skill";
 import Work from "./components/Work";
 import Review from "./components/Review";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -19,6 +22,7 @@ const App = () => {
         <Review />
         <Contact />
       </main>
+      <Footer />
     </>
   );
 };
